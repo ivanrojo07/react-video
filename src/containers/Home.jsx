2ef80f08b7e7d6eace8642be73660d156d02299e
Ivan Rojo <ivanrojo07@gmail.com>
2021-09-30 12:52:27 -0500

    Primer ruta Home con react-router-dom

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,11 +12,11 @@ import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = ()=>{
+const Home = ()=>{
     const videos = useInitialState(API,[])
     
     return (
-        <div className="App">
+        <div className="Home">
             <Header />
             <Search />
             {
@@ -59,4 +59,4 @@ const App = ()=>{
     )
 }
 
-export default App
+export default Home
